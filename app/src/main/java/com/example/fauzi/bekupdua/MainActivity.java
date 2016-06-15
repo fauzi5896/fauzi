c33280c43865c38556c2e9bbac227e8fb7d7f560
fauzi <fauzialam5@gmail.com>
2016-06-15 22:25:23 +0700

    calculator

--- app/src/main/java/com/example/fauzi/bekupdua/MainActivity.java
+++ app/src/main/java/com/example/fauzi/bekupdua/MainActivity.java
@@ -8,20 +8,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnJumlah,btnHitung;
+    Button btnJumlah, btnHitung;
     TextView input1, input2, hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coba);
-        input1= (TextView) findViewById(R.id.angka1);
-        input2= (TextView) findViewById(R.id.angka2);
-        hasil= (TextView) findViewById(R.id.hasil);
+        input1 = (TextView) findViewById(R.id.angka1);
+        input2 = (TextView) findViewById(R.id.angka2);
+        hasil = (TextView) findViewById(R.id.hasil);
 
-        btnHitung= (Button) findViewById(R.id.hitung);
+        btnHitung = (Button) findViewById(R.id.hitung);
 
-        }
+    }
+    //buat di activity_main2
+    //mButton=(Button)findViewById(R.id.btn_satu);
+    //final TextView txtchange = (TextView)findViewById(R.id.alamat);
+
+    //mButton.setOnClickListener(new View.OnClickListener() {
+    //  @Override
+    // public void onClick(View v) {
+    //   txtchange.setText("berubah");
+    //}
+    // });
 
 
 }
